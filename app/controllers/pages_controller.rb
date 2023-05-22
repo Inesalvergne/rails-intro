be87bac19dc65d86515717dd24b0325975444428
Ines Alvergne <i.alvergne@gmail.com>
2023-05-22 10:58:53 -0400

    cleaning code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,16 @@
 class PagesController < ApplicationController
-
   def about; end
 
   def contact
+    # By defining an instance variable we make it available in our corresponding view file
     @students = ['William', 'Emily', 'Pablo', 'Tom', 'Alexane']
     @query = params['member']
     if @query.present?
       @students = @students.select do |student|
         student == @query
+      end
     end
-    end
-    # raise
+    # Use "raise" when you need to open the console to debug your code!
   end
 
   def home
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # The root prefix is used to define the action and page that we load on the base URL
   root 'pages#home', as: :home
+  # We can give aliases to our routes to change the names of the prefix
   get '/about', to: 'pages#about', as: :about
   get '/contact', to: 'pages#contact'
+
+  # 💡 Run "rails routes" in your terminal to get a view of all your routes!
 end
